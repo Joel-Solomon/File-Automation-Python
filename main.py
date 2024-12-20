e15a3173fbd5d941a9da4714eb019e496de5b2e1
Joel Solomon <joelsolomonk@gmail.com>
2024-12-20 19:49:56 +0000

    experimnetation with watchdogs module using filecreatedevent and loggingeventhandler

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+from watchdog.events import FileCreatedEvent
 
 
 '''
@@ -9,6 +15,35 @@ Write down the logical objective and then GOOGLE!!
 4. If the file type is DISTINCT and DOESNT ALREADY EXIST it will create a new file
 '''
 
+if __name__ == "__main__":
+    # Set the format for logging info
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Set format for displaying path
+    #path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = "/Users/joels/Downloads"
+
+    # Initialize logging event handler
+    event_handler = LoggingEventHandler()
+    #file_creation_handler = FileCreatedEvent('/Users/joels/Downloads')
+
+    # Initialize Observer
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    #observer.schedule(file_creation_handler, path, recursive=True)
+
+    # Start the observer
+    observer.start()
+    try:
+        while True:
+            # Set the thread sleep time
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
 
 
 
@@ -18,9 +53,8 @@ Write down the logical objective and then GOOGLE!!
 
 
 #---------------------------------------
-#os.listdir(.C:\Users\joels\Downloads) returns just the contents of a file / directory but not the file type 
+#os.listdir(.C:\Users\joels\Downloads) returns just the contents of a file / directory but not the file type
 file_list = os.scandir('/Users/joels/Downloads') # returns the file type ASWELL as the files in the directory
-
 
 num_of_files = 0
 
